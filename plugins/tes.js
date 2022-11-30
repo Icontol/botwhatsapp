d852a4be8271121e199bf7d0e02d32c11398a955
Icontol <119477979+Icontol@users.noreply.github.com>
2022-12-01 00:04:02 +0700

    Update tes.js

--- plugins/tes.js
+++ plugins/tes.js
@@ -5,13 +5,7 @@ let tesbot = fs.readFileSync('./mp3/tes.opus')
 conn.sendFile(m.chat, tesbot, '', '', m, true)
 }
 
-handler.customPrefix = /(tes?)/i
+handler.customPrefix = /^(tes)$/i
 handler.command = new RegExp
 
-handler.limit = true
-handler.mods = false 
-handler.premium = false
-handler.group = false
-handler.private = false
-
-module.exports = handler
+export default handler
